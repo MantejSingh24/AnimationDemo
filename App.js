04f64a5985bde37fe03865d37df2d78a688c3b2c
Trigbit-06 <dev@trigbit.com>
2020-08-23 18:24:14 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+import Login from './src/login';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isReady: false,
+    };
+  }
+
+  render() {
+    return <Login />;
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
