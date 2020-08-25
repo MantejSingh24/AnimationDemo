ff0f5e5776f65a30c1e17ebeb19cc69b0f222527
Trigbit-06 <dev@trigbit.com>
2020-08-25 18:11:40 +0530

    new commits

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- src/login.js
+++ src/login.js
@@ -209,7 +209,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   button: {
-    backgroundColor: 'white',
+    backgroundColor: '#fff',
     height: 70,
     marginHorizontal: 20,
     borderRadius: 35,
